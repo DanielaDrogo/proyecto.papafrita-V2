df6e2951a4f44494d318aabcd2150487c44d6630
Daniela Drogo <danieladrogo@hotmail.com>
2025-05-29 16:36:39 -0300

    cambios menores en el toastifi

--- js/app.js
+++ js/app.js
@@ -54,16 +54,16 @@ botonesAgregar.forEach((boton, index) => {
         agregarAlCarrito(index);
 
         Toastify({
-        text: "This is a toast",
+        text: "producto agregado al carrito",
         duration: 3000,
         destination: "https://github.com/apvarun/toastify-js",
         newWindow: true,
         close: true,
         gravity: "top", // `top` or `bottom`
-        position: "left", // `left`, `center` or `right`
+        position: "right", // `left`, `center` or `right`
         stopOnFocus: true, // Prevents dismissing of toast on hover
         style: {
-          background: "linear-gradient(to right, #00b09b, #96c93d)",
+          background: "linear-gradient(to right,rgb(148, 0, 0),rgb(48, 29, 0))",
         },
         onClick: function(){} // Callback after click
       }).showToast();
